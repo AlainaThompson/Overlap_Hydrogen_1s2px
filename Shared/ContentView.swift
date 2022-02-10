a490f20ebfbb4640d3db180be066359c880700a6
Alaina Thompson <athompson9@hawk.iit.edu>
2022-02-10 11:10:27 -0600

    overlap_hydrogen_1s2px

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var SModel = OverlapInt()
+    @State var RString = "0.0"
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        VStack{
+                  Text("Atomic Spacing [Å]")
+                      .padding(.top,25)
+                      .padding(.bottom, 0)
+                  TextField("Enter R value [Å]", text: $RString, onCommit: {Task.init {await self.calculateOverlapInt()}})
+                      .padding(.horizontal)
+                      .frame(width: 100)
+                      .padding(.top, 0)
+                      .padding(.bottom, 30)
+                  
+                  
+                
+                  
+                  
+                  
+                  HStack {
+                      VStack{
+                  Text("Overlap Integral")
+                      .padding(.bottom, 0)
+                  TextField("", text: $SModel.SText)
+                      .padding(.horizontal)
+                      .frame(width: 100)
+                      .padding(.top, 0)
+                      .padding(.bottom,30)
+                  
+                        
+                          
+                          
+              }
+              VStack{
+                  Text("Overlap Integral")
+                      .padding(.bottom, 0)
+                  Text("\(SModel.S, specifier: "%.2f")")
+                      .padding(.horizontal)
+                      .frame(width: 100)
+                      .padding(.top, 0)
+                      .padding(.bottom,30)
+                      
+            
+                  
+                  
+                  
+                  
+                  
+                  
+                  }
+                  
+                  }
+                    
+                  
+                  Button("Calculate", action: {Task.init { await self.calculateOverlapInt() }})
+                      .padding(.bottom)
+                      .padding()
+                      .disabled(SModel.enableButton == false)
+                      
+                  
+                  
+              }
+              
     }
-}
+              
+          func calculateOverlapInt() async {
+              
+              SModel.setButtonEnable(state: false)
+              
+              let _ : Bool = await SModel.initWithOverlap(R: Double(RString)!)
+              
+              
+
+      }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    }
+
+
 }
---
+++ Shared/Overlap Hydrogen.swift
@@ -0,0 +1,136 @@
+//
+//  Overlap Hydrogen.swift
+//  Overlap_Hydrogen_1s2px
+//
+//  Created by Alaina Thompson on 2/10/22.
+//
+
+import Darwin
+import SwiftUI
+
+class OverlapInt: NSObject,ObservableObject {
+  
+    let e = Darwin.M_E
+    var atomicSpacing = 0.0
+    var a = 0.529177
+    var n = 0.0
+    @Published var S = 0.0
+    @Published var SText = ""
+    @Published var RString = "0.0"
+    @Published var enableButton = true
+    
+    
+
+
+
+  
+    
+    
+    
+    
+    func initWithOverlap(R: Double) async -> Bool {
+            // a and R are in angstroms
+            atomicSpacing = R
+                
+                let _ = await withTaskGroup(of:  Void.self) { taskGroup in
+                    
+            
+                
+                    taskGroup.addTask { let _ = await self.calculateS(R: self.atomicSpacing)}
+                    
+            }
+                
+                await setButtonEnable(state: true)
+                                                     
+           
+            
+
+            return true
+            
+        }
+    
+    
+    
+    
+    func calculateS(R: Double) async -> Double {
+        
+    
+        //          - R /  a                   2
+        //                            R       R     R
+        // S(R)  =   e       (1 +   ---- +  -----)(---)
+        //                           a         2    a
+        //                                  3 a
+        //
+        n = R/a
+        let exp = pow(e, -n)
+        let frac1 = n
+        let frac2 = pow(n, 2)/3
+                
+        let calculatedS = n*(1 + frac1 + frac2)*exp
+        let newSText = String(format: "%7.5f", calculatedS)
+        
+        await updateS(STextString: newSText)
+        await newSValue(SValue: calculatedS)
+               
+        return calculatedS
+               
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    @MainActor func setButtonEnable(state: Bool){
+               
+               
+               if state {
+                   
+                   Task.init {
+                       await MainActor.run {
+                           
+                           
+                           self.enableButton = true
+                       }
+                   }
+                   
+                   
+                       
+               }
+               else{
+                   
+                   Task.init {
+                       await MainActor.run {
+                           
+                           
+                           self.enableButton = false
+                       }
+                   }
+                       
+               }
+               
+           }
+           
+          
+           @MainActor func updateS(STextString: String){
+               
+               SText = STextString
+               
+           }
+           
+           @MainActor func newSValue(SValue: Double){
+               
+               self.S = SValue
+               
+           }
+           
+
+    
+    
+    
+    
+    
+    
+}
